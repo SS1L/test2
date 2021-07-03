4310de3c90b4afcd3d6cda849bd22c2ae6e84a26
SS1L <vlad7art13@gmail.com>
2021-07-03 13:19:53 +0300

    solve

--- index.js
+++ index.js
@@ -1,72 +1,72 @@
 const matrix = [
-    ['#', '+', '+', '+', '#', '#', '#', '#', '#'],
-    ['#', '+', '#', '+', '#', '+', '+', '+', '+'],
-    ['#', '+', '#', '+', '+', '+', '#', '#', '#'],
-    ['+', '+', '#', '#', '0', '+', '#', '+', '#'],
-    ['#', '#', '#', '+', '+', '#', '#', '#', '#'],
-    ['#', '#', '+', '+', '+', '#', '#', '#', '#'],
-    ['#', '#', '+', '#', '+', '#', '#', '#', '#'],
-    ['#', '#', '#', '#', '+', '#', '#', '#', '#'],
+  ['#', '+', '+', '+', '#', '#', '#', '#', '#'],
+  ['#', '+', '#', '+', '#', '+', '+', '+', '#'],
+  ['#', '+', '#', '+', '+', '+', '#', '#', '#'],
+  ['+', '+', '#', '#', '0', '+', '#', '+', '#'],
+  ['#', '#', '#', '+', '+', '#', '#', '#', '#'],
+  ['#', '#', '+', '+', '+', '#', '#', '#', '#'],
+  ['#', '#', '+', '#', '+', '#', '#', '#', '#'],
+  ['#', '#', '#', '#', '#', '#', '#', '#', '#'],
 ];
-console.log(matrix[0][0]);
-const row = matrix.findIndex((row) => row.includes('0'));
+
+const row = matrix.findIndex((r) => r.includes('0'));
 const column = matrix[row].indexOf('0');
 const start = [row, column];
-const end = [7, 4];
-console.log(matrix.length)
+const end = [3, 0];
+
 if (end[1] === 0) {
-    end[1] -= 1;
+  end[1] -= 1;
 } else if (end[1] === matrix.length) {
-    end[1] += 1;
+  end[1] += 1;
 } else if (end[0] === 0) {
-    end[0] -= 1;
+  end[0] -= 1;
 } else if (end[0] === matrix.length - 1) {
-    end[0] += 1;
+  end[0] += 1;
 }
+
 function findWay(position, end) {
-    let arr = [];
-    matrix[position[0]][position[1]] = '-';
+  const arr = [];
+  matrix[position[0]][position[1]] = '-';
 
-    arr.push([position]);
+  arr.push([position]);
 
-    while (arr.length > 0) {
-        let path = arr.shift();
-        let pos = path[path.length - 1];
-        let direction = [
-            [pos[0] - 1, pos[1], { answer: 'up' }],
-            [pos[0], pos[1] + 1, { answer: 'right' }],
-            [pos[0] + 1, pos[1], { answer: 'down' }],
-            [pos[0], pos[1] - 1, { answer: 'left' }]
-        ];
+  while (arr.length > 0) {
+    const path = arr.shift();
+    const pos = path[path.length - 1];
+    const direction = [
+      [pos[0] - 1, pos[1], { answer: 'up' }],
+      [pos[0], pos[1] + 1, { answer: 'right' }],
+      [pos[0] + 1, pos[1], { answer: 'down' }],
+      [pos[0], pos[1] - 1, { answer: 'left' }],
+    ];
 
-        for (let i = 0; i < direction.length; i++) {
-            if (direction[i][0] == end[0] && direction[i][1] == end[1]) {
-                return path.concat([end]);
-            }
+    for (let i = 0; i < direction.length; i++) {
+      if (direction[i][0] === end[0] && direction[i][1] === end[1]) {
+        return path.concat([end]);
+      }
 
-            if (direction[i][0] < 0 || direction[i][0] >= matrix[0].length
-                || direction[i][1] < 0 || direction[i][1] >= matrix[0].length
-                || matrix[direction[i][0]][direction[i][1]] != '+') {
-                continue;
-            }
+      if (direction[i][0] < 0 || direction[i][0] >= matrix[0].length
+         || direction[i][1] < 0 || direction[i][1] >= matrix[0].length
+         || matrix[direction[i][0]][direction[i][1]] !== '+') {
+        continue;
+      }
 
-            matrix[direction[i][0]][direction[i][1]] = '-';
-            arr.push(path.concat([direction[i]]));
-        }
+      matrix[direction[i][0]][direction[i][1]] = '-';
+      arr.push(path.concat([direction[i]]));
     }
+  }
 }
 
-let path = findWay(start, end);
+const path = findWay(start, end);
 if (!path) {
-    console.log('Ohh I`m stack');
-    console.log(matrix)
+  console.log('Ohh I`m stack');
+  console.log(matrix);
 } else {
-    let answer = [];
-    path.forEach((index) => {
-        if (index[2] !== undefined) {
-            answer.push(index[2].answer)
-        }
-    })
-    console.log(answer)
+  const answer = [];
+  path.forEach((index) => {
+    if (index[2] !== undefined) {
+      answer.push(index[2].answer);
+    }
+  });
+  console.log(answer);
 }
-//console.log(JSON.stringify(path));
