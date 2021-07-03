3c1191e8ea1f4144de9fd8db6bc2e547244c5eb2
SS1L <vlad7art13@gmail.com>
2021-07-03 11:43:03 +0300

    maze

--- index.js
+++ index.js
@@ -1,1 +1,63 @@
-console.log('First commit');
+const matrix = [
+    ['#', '+', '+', '+', '#', '#', '#', '#', '#'],
+    ['#', '+', '#', '+', '#', '+', '+', '+', '#'],
+    ['#', '+', '#', '+', '+', '+', '#', '+', '#'],
+    ['+', '+', '#', '#', '0', '+', '#', '+', '#'],
+    ['#', '#', '#', '+', '+', '#', '#', '#', '#'],
+    ['#', '#', '+', '+', '#', '#', '#', '#', '#'],
+    ['#', '#', '+', '#', '#', '#', '#', '#', '#'],
+    ['#', '#', '#', '#', '#', '#', '#', '#', '#'],
+    ['#', '#', '#', '#', '#', '#', '#', '#', '#'],
+];
+const row = matrix.findIndex((row) => row.includes('0'));
+const column = matrix[row].indexOf('0');
+const start = [row, column];
+const end = [0, 4];
+
+function findWay(position, end) {
+    let arr = [];
+    matrix[position[0]][position[1]] = '-';
+
+    arr.push([position]); 
+
+    while (arr.length > 0) {
+        let path = arr.shift();
+        let pos = path[path.length - 1]; 
+        let direction = [
+            [pos[0] - 1, pos[1], { answer: 'up' }],
+            [pos[0], pos[1] + 1, { answer: 'right' }],
+            [pos[0] + 1, pos[1], { answer: 'down' }],
+            [pos[0], pos[1] - 1, { answer: 'left' }]
+        ];
+
+        for (let i = 0; i < direction.length; i++) {
+            if (direction[i][0] == end[0] && direction[i][1] == end[1]) {
+                return path.concat([end]);
+            }
+
+            if (direction[i][0] < 0 || direction[i][0] >= matrix[0].length
+                || direction[i][1] < 0 || direction[i][1] >= matrix[0].length
+                || matrix[direction[i][0]][direction[i][1]] != '+') {
+                continue;
+            }
+
+            matrix[direction[i][0]][direction[i][1]] = '-';
+            arr.push(path.concat([direction[i]]));
+        }
+    }
+}
+
+let path = findWay(start, end);
+if (!path) {
+    console.log('Ohh I`m stack');
+    console.log(matrix)
+} else {
+    let answer = [];
+    path.forEach((index) => {
+        if (index[2] !== undefined){
+            answer.push(index[2].answer)
+        }
+    })
+    console.log(answer)
+}
+//console.log(JSON.stringify(path));
